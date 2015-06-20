685815d61bc1dc33ce7de2751a495ea7c0ade132
ryan <ilmaestro@gmail.com>
2015-06-20 13:52:46 -0700

    cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,13 @@
 var gulp = require('gulp'),
-    //watch = require('gulp-watch'),
-    //shell = require('gulp-shell'),
     run = require('gulp-run'),
     gutil = require('gulp-util'),
     connect = require('gulp-connect');
-
-var fsi = new run.Command('fsharpi');
 
 gulp.task('default', ['fs', 'webserver', 'watch']);
 
 gulp.task('fs', function(cb) {
     gutil.log("Compiling F# Scripts...");
     run("fsharpi ./build.fsx").exec([], cb);
-        //.on('end', cb);
 });
 
 gulp.task('watch', function() {
@@ -21,8 +16,6 @@ gulp.task('watch', function() {
 
 gulp.task('webserver', function() {
   connect.server({
-    root: 'wwwroot',
-    port: 8888,
-    livereload: true
+    root: 'wwwroot'
   });
 });
